1c9b5b6ead0ab086da6e0a026ffa68a684fef78c
satheeshmk15 <128846933+satheeshmk15@users.noreply.github.com>
2023-03-25 01:23:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket SMoothie')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard Boiled Free-Range Egg')
 
